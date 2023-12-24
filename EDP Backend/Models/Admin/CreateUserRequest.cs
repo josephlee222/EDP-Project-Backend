4e286be86b274712140a028bb56bfbcfda660974
Joseph Lee <facebooklee52@gmail.com>
2023-12-24 23:31:23 +0800

    Change error message

--- EDP Backend/Models/Admin/CreateUserRequest.cs
+++ EDP Backend/Models/Admin/CreateUserRequest.cs
@@ -5,7 +5,7 @@ namespace EDP_Backend.Models
     public class CreateUserRequest
     {
         [MinLength(3), MaxLength(64), Required]
-        [RegularExpression(@"^[a-zA-Z '-,.]+$", ErrorMessage = "Only allow letters, spaces and characters: ' - , .")]
+        [RegularExpression(@"^[a-zA-Z '-,.]+$", ErrorMessage = "Only letters, spaces and characters: ' - , . are accepted")]
         public string Name { get; set; } = string.Empty;
         [MaxLength(128), EmailAddress, Required]
         public string Email { get; set; } = string.Empty;
