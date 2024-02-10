3e6a3be964f8922c6739da4c91c48d871baa973f
Joseph Lee <facebooklee52@gmail.com>
2024-02-11 01:09:02 +0800

    Completed cart

---
+++ EDP Backend/Migrations/20240210033603_contact-information.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EDP_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class contactinformation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthday",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nric",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Nric",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Bookings");
+        }
+    }
+}
