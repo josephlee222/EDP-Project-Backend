90a7233e26fd2c0659e223efe97a0cb479158c25
Joseph Lee <facebooklee52@gmail.com>
2024-02-12 17:36:26 +0800

    Group discussions

---
+++ EDP Backend/Hubs/GroupsHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace EDP_Backend.Hubs
+{
+
+    public class GroupsHub : Hub
+    {
+        public async Task Register(int id)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, id.ToString());
+            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", "Connected " + id);
+        }
+    }
+}
